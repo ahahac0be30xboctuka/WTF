512b001dc266b97ca86a3f18a58c0f0ef32ad1c9
ahahac0be30xboctuka <157404929+ahahac0be30xboctuka@users.noreply.github.com>
2024-01-23 06:24:55 +0300

    Update Emo.java

--- src/StoryWorld/Alive/Animals/Emotions/Emo.java
+++ src/StoryWorld/Alive/Animals/Emotions/Emo.java
@@ -1,11 +1,8 @@
-package StoryWorld.Alive.Animals.Emotions;
+ackage StoryWorld.Alive.Animals.Emotions;
 
 public enum Emo {
     HAPPINESS("счастье", 10, -15),
-    SADNESS("печаль", -10, 5),
     FEAR("страх", -10, 10),
-    ANGER("злость", -15, 12),
-    LOVE("любовь", 70, -10),
     RELIEF("облегчение", 25, -30);
 
     private final String emotion;
