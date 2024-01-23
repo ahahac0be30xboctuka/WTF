7c758fe83029120b4464df1c451a13a3f1c21ef1
ahahac0be30xboctuka <157404929+ahahac0be30xboctuka@users.noreply.github.com>
2024-01-23 06:22:47 +0300

    Update EmotionsAction.java

--- src/StoryWorld/Alive/Human/Emotions/EmotionsAction.java
+++ src/StoryWorld/Alive/Human/Emotions/EmotionsAction.java
@@ -4,6 +4,4 @@ import java.util.ArrayList;
 
 public interface EmotionsAction {
     void feel(ArrayList<Emo> emotion);
-    void show(ArrayList<Emo> emotions);
-    void hide(ArrayList<Emo> emotion);
 }
