a9b0fac3b984551f9991c8ac09759143738ca36e
ahahac0be30xboctuka <157404929+ahahac0be30xboctuka@users.noreply.github.com>
2024-01-23 06:19:40 +0300

    Update HandsActions.java

--- src/StoryWorld/Alive/Human/HandsActions.java
+++ src/StoryWorld/Alive/Human/HandsActions.java
@@ -3,22 +3,21 @@ package StoryWorld.Alive.Human;
 import StoryWorld.Alive.WildlifeObjects;
 import StoryWorld.Exceptions.NotEnoughMoneyException;
 import StoryWorld.Inanimate.InanimateObjects;
-import StoryWorld.Inanimate.Paper;
 
 public interface HandsActions {
-    void put(InanimateObjects object, InanimateObjects on);
     void put(WildlifeObjects object, InanimateObjects on);
 
     void take(InanimateObjects object);
 
     void take(WildlifeObjects object);
 
-    void write(Paper on, String text);
+    void write(InanimateObjects.Paper paper, String text);
 
     void attach(InanimateObjects object, InanimateObjects to);
+
     void bring(InanimateObjects object);
-    void make(WildlifeObjects object);
+
     void make(InanimateObjects object);
-    void buy(WildlifeObjects object, int cost) throws NotEnoughMoneyException;
+
     void buy(InanimateObjects object, int cost) throws NotEnoughMoneyException;
 }
