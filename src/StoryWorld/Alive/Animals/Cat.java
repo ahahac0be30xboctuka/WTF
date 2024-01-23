59357ff5f82773024efc4101ae766d4fda847fb0
ahahac0be30xboctuka <157404929+ahahac0be30xboctuka@users.noreply.github.com>
2024-01-23 06:24:14 +0300

    Update Cat.java

--- src/StoryWorld/Alive/Animals/Cat.java
+++ src/StoryWorld/Alive/Animals/Cat.java
@@ -2,71 +2,42 @@ package StoryWorld.Alive.Animals;
 
 import StoryWorld.Alive.Animals.Emotions.Emo;
 import StoryWorld.Alive.Animals.Emotions.Emotions;
-import StoryWorld.Alive.Gender;
+import StoryWorld.Alive.Human.Gender;
 import StoryWorld.Alive.WildlifeObjects;
+import StoryWorld.Exceptions.AgeException;
 import StoryWorld.Inanimate.InanimateObjects;
 import StoryWorld.Places.Place;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class Cat extends WildlifeObjects implements Emotions {
     private int age;
-    public String name;
-    public ArrayList<Cat> children = new ArrayList<>();
-    private Gender gender;
-    private int mood;
-    private int tiredness;
-    private ArrayList<WildlifeObjects> allowedList = new ArrayList<>();
+    private final Gender gender;
+    int mood;
+    int tiredness;
 
-    public Cat(String name) {
-        super(name);
-        this.setAge(age);
-    }
-
-    public Cat(String name, int age, Gender gender) {
-        super(name);
-        this.setAge(age);
-        this.children = null;
-        this.gender = gender;
-    }
-
-    public Cat(String name, int age, Gender gender, Place location) {
+    public Cat(String name, int age, Gender gender, Place location) throws AgeException {
         super(name, location);
         this.age = age;
-        this.children = null;
         this.gender = gender;
-    }
-
-    public Cat(String name, int age, Gender gender, Place location, ArrayList<Cat> children) {
-        super(name, location);
-        this.age = age;
-        this.children = children;
-        this.gender = gender;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     private String verbEnding(String verb) {
         if (!gender.equals(Gender.MALE)) {
             switch (verb) {
-                case "подошел":
-                    verb = "подощла";
-                    break;
-                case "мяукнул":
-                    verb = "мяукнула";
-                    break;
-                case "позволил":
-                    verb = "позволила";
-                    break;
-                case "изменился":
-                    verb = "изменилась";
-                    break;
+                case "подошел" -> verb = "подощла";
+                case "мяукнул" -> verb = "мяукнула";
+                case "позволил" -> verb = "позволила";
+                case "изменился" -> verb = "изменилась";
             }
         }
         return verb;
+    }
+
+    public void setAge(int age) throws AgeException {
+        if (age < 0 || age > 20) throw new AgeException("Некорректный возраст животного age = " + age);
+        this.age = age;
     }
 
     public void walk(WildlifeObjects to) {
@@ -84,7 +55,10 @@ public class Cat extends WildlifeObjects implements Emotions {
     }
 
     public void sleep() {
-        System.out.println(name + "спит");
+        feel(new ArrayList<>(List.of(Emo.RELIEF)));
+        if (aliveinteraction) {
+            System.out.println(getName() + " спит");
+        }
     }
 
     public void say(WildlifeObjects object, String text) {
@@ -101,21 +75,13 @@ public class Cat extends WildlifeObjects implements Emotions {
 
     public void allowToCarryMe(WildlifeObjects object) {
         String verb = "позволил";
-        this.allowedList.add(object);
-        feel(new ArrayList<>(Arrays.asList(Emo.HAPPINESS)));
+        feel(new ArrayList<>(List.of(Emo.HAPPINESS)));
         System.out.printf("\"%s\" %s \"%s\" таскать себя%n", getName(), verbEnding(verb), object.getName());
-    }
-
-    public void disallowToCarryMe() {
-        this.allowedList.clear();
-        feel(new ArrayList<>(Arrays.asList(Emo.ANGER)));
-        System.out.printf("\"%s\" не даёт таскать себя%n", getName());
     }
 
     public void change() {
         String verb = "изменился";
-        this.tiredness = 100;
-        this.mood = 100;
+        feel(new ArrayList<>(List.of(Emo.FEAR)));
         System.out.printf("\"%s\" %s%n", getName(), verbEnding(verb));
     }
 
@@ -132,14 +98,5 @@ public class Cat extends WildlifeObjects implements Emotions {
     @Override
     public void feel(ArrayList<StoryWorld.Alive.Animals.Emotions.Emo> emotion) {
         expressEmotions("почувствовал", emotion);
-    }
-
-    @Override
-    public void show(ArrayList<Emo> emotion) {
-        expressEmotions("показал", emotion);
-    }
-    @Override
-    public void hide(ArrayList<Emo> emotion) {
-        expressEmotions("cкрыл", emotion);
     }
 }
